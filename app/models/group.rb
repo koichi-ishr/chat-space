9dbe96762d235e1b69d3d0f4c989e5a132fcd9fe
koichi-ishr <72620229+koichi-ishr@users.noreply.github.com>
2020-11-24 19:15:54 +0900

    create how to send message

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,18 @@
 class Group < ApplicationRecord
   has_many :group_users
   has_many :users, through: :group_users
+  has_many :messages
   validates :name, presence: true, uniqueness: true
+
+  def show_new_message
+    if (new_message = messages.last).present?
+      if new_message.content?
+        new_message.content
+      else
+        "画像が投稿されています。"
+      end
+    else
+      "まだメッセージがありません。"
+    end
+  end
 end
